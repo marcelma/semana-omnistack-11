daf128b21d166b224e5c512d17bd98b96cd4d70a
Marcell Martini <marcellmartini@gmail.com>
2020-03-27 22:32:16 -0400

    feat:  Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Route from './src/routes';
+
+export default function App() {
+  return (
+    <Route />
+  );
+}
